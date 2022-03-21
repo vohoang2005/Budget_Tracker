44166d721193816a0ae8e5aa13641c1dc1009ad7
vohoang2005 <vohoang2005@gmail.com>
2022-03-20 21:53:03 -0700

    updated connection

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Budget_Tracker";
 
 const app = express();
 
@@ -15,13 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true
+  
 });
 
+// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
